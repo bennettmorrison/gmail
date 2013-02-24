96c9a609ae514c9e0057089b117cf838ee4a9dba
Ryan Chan <ryan@ryanlchan.com>
2013-02-23 21:49:21 -0800

    Use label_data from mexisme/gmail-imap
    Much cleaner way of parsing label strings using IMAP's built-in ASTRING
    parser.

--- lib/gmail/client/imap_extensions.rb
+++ lib/gmail/client/imap_extensions.rb
@@ -36,7 +36,7 @@ module GmailImapExtensions
           # Gmail extension additions.
           # Cargo-Cult code warning: # I have no idea why the regexp - just copying a pattern
           when /\A(?:X-GM-LABELS)\z/ni
-            name, val = x_gm_labels_data
+            name, val = label_data
           when /\A(?:X-GM-MSGID)\z/ni 
             name, val = uid_data
           when /\A(?:X-GM-THRID)\z/ni 
@@ -49,53 +49,28 @@ module GmailImapExtensions
         return attr
       end
 
-      # Based on Net::IMAP#flags_data, but calling x_gm_labels_list to parse labels
-      def x_gm_labels_data
+      def label_data
         token = match(self.class::T_ATOM)
         name = token.value.upcase
         match(self.class::T_SPACE)
-        return name, x_gm_label_list
+        return name, astring_list
       end
 
-      # Based on Net::IMAP#flag_list with a modified Regexp
-      # Labels are returned as escape-quoted strings
-      # We extract the labels using a regexp which extracts any unescaped strings
-      def x_gm_label_list
-        if @str.index(/\(([^)]*)\)/ni, @pos)
-          resp = extract_labels_response
-
-          # We need to manually update the position of the regexp to prevent trip-ups
-          @pos += resp.length
-          return resp.scan(/"([^"\\]*(?:\\.[^"\\]*)*)"/ni).flatten.collect(&:unescape)
-        else
-          parse_error("invalid label list")
-        end
-      end
-
-      # The way Gmail return tokens can cause issues with Net::IMAP's reader,
-      # so we need to extract this section manually
-      def extract_labels_response
-        special, quoted = false, false
-        index, paren_count = 0, 0
-
-        # Start parsing response string for the labels section, parentheses inclusive
-        labels_header = "X-GM-LABELS ("
-        start = @str.index(labels_header) + labels_header.length - 1
-        substr = @str[start..-1]
-        substr.each_char do |char|
-          index += 1
-          case char
-          when '('
-            paren_count += 1 unless quoted
-          when ')'
-            paren_count -= 1 unless quoted
-            break if paren_count == 0
-          when '"'
-            quoted = !quoted unless special
+      def astring_list
+        result = []
+        match(self.class::T_LPAR)
+        while true
+          token = lookahead
+          case token.symbol
+          when self.class::T_RPAR
+            shift_token
+            break
+          when self.class::T_SPACE
+            shift_token
           end
-          special = (char == '\\' && !special)
+          result.push(astring)
         end
-        substr[0..index]
+        return result
       end
     end # class_eval
 
